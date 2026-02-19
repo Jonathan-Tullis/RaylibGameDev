23f96a106bc56649a4fa60c76afd5d2b3e39072c
Jonathan-Tullis <jonathantullis171@gmail.com>
2026-02-19 09:53:18 -0700

    Added Left and Right movement, as well as bounding coordinates.

--- Project/axe_game.cpp
+++ Project/axe_game.cpp
@@ -11,6 +11,9 @@ int main()
     int circle_x{width/2};
     int circle_y{height/2};
 
+
+    SetTargetFPS(60);
+
     while (WindowShouldClose() == false)
     {
         BeginDrawing();
@@ -18,6 +21,24 @@ int main()
 
         // Game logic begins
         DrawCircle(circle_x, circle_y, 25, BLUE);
+
+        /*
+         Move Right and bound it to 350 since the right most coordinate is 350,0
+         Since we have a variable called width which may change, we use it instead of just 350
+        */
+        if (IsKeyDown(KEY_D) && circle_x < width)
+        {
+            circle_x = circle_x + 10;
+        }
+
+        /*
+         Move left and bound it to 0 since the left coordinate is 0,0
+        */
+        if (IsKeyDown(KEY_A) && circle_x > 0)
+        {
+            circle_x = circle_x - 10;
+        }
+
 
         // Game logic ends
         EndDrawing();
